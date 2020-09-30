dd56adce8637871b6a29ea4924fd9b622f28809b
ashsei <58061442+ashsei@users.noreply.github.com>
2020-09-30 16:47:19 -0600

    Base Signin Functionality Implemented and Operating

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Redirect } from 'react-router-dom';
 import Layout from '../core/Layout';
+import { signin } from '../auth'
 
-const Signin = () => 
-    <Layout title="Sign In" description="Sign In to your Deerhart Designs account!">
-        ...
-    </Layout>
+const Signin = () => {
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        error: '',
+        loading: false,
+        redirectToReferrer: false,
+    });
 
+    const {email, password, error, loading, redirectToReferrer} = values
+
+    const handleChange = name => event => {
+        setValues({...values, error: false, [name]: event.target.value});
+    };
+
+    const clickSubmit = (event) => {
+        event.preventDefault();
+        setValues({...values, error: false, loading: true});
+        signin({ email, password })
+        .then(data => {
+            if(data.error) {
+                setValues({...values, error: data.error, loading: false});
+            } else {
+                setValues({...values, redirectToReferrer: true, loading: false});
+            };
+        })
+    };
+
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input onChange={handleChange('email')} type="text" className="form-control"
+                value={email}/>
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input onChange={handleChange('password')} type="password" className="form-control" value={password}/>
+                {/* !!! Add option for user to see password !!!  */}
+            </div>
+            <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
+        </form>
+    );
+
+    const showError = () => (
+        <div className="alert alert-danger" style= {{display: error ? '' : 'none'}}>
+            {error}
+        </div>
+    );
+
+    const showLoading = () => (
+        loading && (
+            <div className="alert alert-info" style= {{display: loading ? '' : 'none'}}>
+                <h2>Loading...</h2>
+            </div>
+        )
+    );
+
+    const redirectUser = () => {
+        if (redirectToReferrer) {
+            return <Redirect to="/"/>
+        }
+    }
+
+    return (
+        <Layout 
+            title="Sign Up" 
+            description="Sign Up for a Deerhart Designs account!"
+            className="container col-md-8 offset-md-2"
+        >
+            {showLoading()}
+            {showError()}
+            {signUpForm()}
+            {redirectUser()}
+        </Layout>
+    );
+}
+    
 
 
 export default Signin;
