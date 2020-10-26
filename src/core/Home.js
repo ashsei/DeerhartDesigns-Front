98b49e9c7d0ad06343652db28ba6396d0588e6c9
Ashton Richard Kenneth Seibel <ashton@seibel.life>
2020-10-25 20:36:54 -0600

    Show Most Sold and Most Recently Added Products Methods Implemented on Home Page

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from './Layout';
+import { getProducts } from './apiCore'
 
-const Home = () => 
-    <Layout title="Deerhart Designs" description="Kangaroo Deerhart's Art">
-        ...
-    </Layout>
+const Home = () => {
+    const [productsBySell, setProductsBySell] = useState([])
+    const [productsByArrival, setProductsByArrival] = useState([])
+    const [error, setError] = useState(false)
 
+    const loadProductsBySell = () => {
+        getProducts('sold').then(data => {
+            if(data.error) {
+                setError(data.error)
+            } else {
+                setProductsBySell(data)
+            }
+        })
+    }
+    const loadProductsByArrival = () => {
+        getProducts('createdAt').then(data => {
+            if(data.error) {
+                setError(data.error)
+            } else {
+                setProductsByArrival(data)
+            }
+        })
+    }
+
+    useEffect(()=> {
+        loadProductsByArrival()
+        loadProductsBySell()
+    }, [])
+   return ( 
+        <Layout title="Deerhart Designs" description="Kangaroo Deerhart's Art">
+            {JSON.stringify(productsByArrival)}
+            <hr/>
+            {JSON.stringify(productsBySell)}
+        </Layout>
+    )
+}
 
 
 export default Home;
