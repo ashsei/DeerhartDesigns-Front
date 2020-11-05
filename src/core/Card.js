517e7f04092e7d57c85c7831a28426cbc33535de
Ashton Richard Kenneth Seibel <ashton@seibel.life>
2020-11-04 23:57:11 -0700

    Basic Functionality for Add To Cart Button Implemented

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import ShowImage from './ShowImage';
 import moment from 'moment';
+import {addItem} from './CartHelpers'
 
 const Card = ({ product, showViewProductButton = true}) => {
+    const [redirect, setRedirect] = useState(false)
+    
     const showViewButton = (showViewProductButton) => {
         return (
             showViewProductButton && (
@@ -15,9 +18,20 @@ const Card = ({ product, showViewProductButton = true}) => {
             )
         )
     }
+    const addToCart = () =>{
+        addItem(product, () => {
+            setRedirect(true)
+        })
+    }
+
+    const shouldRedirect = redirect => {
+        if(redirect) {
+            return <Redirect to="/cart" />
+        }
+    }
     const showAddToCartButton = () =>{
         return (
-            <button className="btn btn-outline-warning mt-2 mb-2">Add to Cart</button>
+            <button onClick={addToCart} className="btn btn-outline-warning mt-2 mb-2">Add to Cart</button>
         )
     }
     const showStock = quantity => {
@@ -32,7 +46,8 @@ const Card = ({ product, showViewProductButton = true}) => {
         <div className="card">
             <div className="card-header name">{product.name}</div>
             <div className="card-body">
-            <ShowImage item={product} url="product" />
+                {shouldRedirect(redirect)}
+                <ShowImage item={product} url="product" />
                 <p className="lead mt-2">{product.description.substring(0, 100)}</p>
                 <p className="black-10">${product.price}</p>
                 <p className="black-9">Category: {product.category && product.category.name}</p>
