f1070ca82c4f1f569f0f57d6606dba59d200a552
Ashton Richard Kenneth Seibel <ashton@seibel.life>
2020-11-09 23:25:20 -0700

    Error Handling Implemented for Drop In

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -49,22 +49,41 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
         )
     };
 
+    const buy = () => {
+        let nonce;
+        let getNonce = data.instance.requestPaymentMethod()
+        .then(data => {
+            console.log(data)
+            nonce = data.nonce
+            console.log('send nonce and total to process:', nonce, getTotal(products))
+        })
+        .catch(error => {
+            console.log("Dropin Error: ", error);
+            setData({ ...data, error: error.message });
+        });
+    }
+    
     const showDropIn = () => (
-        <div>
+        <div onBlur={() => setData({...data, error: ""})}>
             {data.clientToken !== null && products.length > 0 ? (
                 <div>
                     <DropIn options={{ 
                         authorization: data.clientToken
                     }} onInstance={instance => (data.instance = instance)} />
-                    <button className="btn btn-success">Checkout</button>
+                    <button onClick={buy} className="btn btn-success">Pay</button>
                 </div>
             ) : null }
         </div>
     )
 
+    const showError = error => (
+        <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>{error}</div>
+    )
+
     return (
         <div>
             <h2>Total: ${getTotal()}</h2>
+            {showError(data.error)}
             {showCheckout()}
         </div>
     )
