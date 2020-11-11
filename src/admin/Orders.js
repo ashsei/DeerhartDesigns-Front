1e8df0d7ff2f09979feaf1b1ee5cbca8d33a5171
Ashton Richard Kenneth Seibel <ashton@seibel.life>
2020-11-10 23:02:07 -0700

    Implemented Ability to Update Order Status from Admin Order Page

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -2,15 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
-import { listOrders } from './apiAdmin';
+import { listOrders, getStatusValues, updateOrderStatus } from './apiAdmin';
 import moment from 'moment';
 
 
 const Orders = () => {
     const [orders, setOrders] = useState([])
+    const [statusValues, setStatusValues] = useState([])
 
     const {user, token} = isAuthenticated()
-    
+
     const loadOrders = () => {
         listOrders(user._id, token).then(data => {
             if(data.error){
@@ -21,8 +22,18 @@ const Orders = () => {
         })
     }
 
+    const loadStatusValues = () => {
+        getStatusValues(user._id, token).then(data => {
+            if(data.error){
+                console.log(data.error)
+            } else {
+                setStatusValues(data)
+            }
+        })
+    }
     useEffect(() => {
         loadOrders();
+        loadStatusValues()
     }, [])
 
     const showOrdersLength = () => {
@@ -43,6 +54,34 @@ const Orders = () => {
             <input type="text" className="form-control" value={value} readOnly />
         </div>
     )
+
+    const handleStatusChange = (e, orderId) => {
+        updateOrderStatus(user._id, token, orderId, e.target.value).then(data => {
+            if(data.error){
+                console.log('Status Update Failed')
+            } else {
+                loadOrders();
+            }
+        })
+    }
+
+    const showStatus = o => (
+        <div className="form-group">
+            <h3 className="mark mb-4">Status: {o.status} </h3>
+            <select 
+                className="form-control" 
+                onChange={e => handleStatusChange(e, o._id)}>
+                    <option>
+                        Update Status
+                    </option>
+                    {statusValues.map((status, index) => (
+                        <option key={index} value={status}>
+                            {status}
+                        </option>
+                    ))}
+            </select> 
+        </div>
+    )
     return (
         <Layout title="Orders" description={`Hello, ${user.name}! Please use this page to manage your orders!`} className="container-fluid">
             <div className="row">
@@ -52,16 +91,16 @@ const Orders = () => {
                         return (
                             <div className="mt-5" key={oIndex} style={{borderBottom: '5px solid indigo'}}>
                                 <h2 className="mb-5"><span className="bg-primary">Order ID: {o._id}</span></h2>
-                            
+
                                 <ul className="list-group">
-                                    <li className="list-group-item">Order Status: {o.status}</li>                            
+                                    <li className="list-group-item">Order Status: {showStatus(o)}</li>                            
                                     <li className="list-group-item">Transaction ID: {o.transaction_id}</li>
                                     <li className="list-group-item">Amount: ${o.amount}</li>
                                     <li className="list-group-item">Ordered By: {o.user.name}</li>
                                     <li className="list-group-item">Ordered On: {moment(o.createdAt).fromNow()}</li>
                                     <li className="list-group-item">Delivery Address: {o.address}</li>
                                 </ul>
-
+                                
                                 <h3 className="mt-4 mb-4 font italic">
                                     Total Produtcs in the Order: {o.products.length}
                                 </h3>
